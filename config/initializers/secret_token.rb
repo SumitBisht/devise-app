bdd5f812983ca57780b1b47f4c10b26cfabbab8c
sumit bisht <sumitbisht1987@gmail.com>
2013-07-11 10:55:46 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleAuth::Application.config.secret_token = '99f93085ed60f367e1ed44a2f0405c40cca6689ca4d240402ce20e0303d93361380a20b27443ee76f0c4408b330f58d59104393070b3b36d703c74c470f7cd5b'
