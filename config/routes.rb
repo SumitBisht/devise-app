11a808479a17d008352c6133ae6619764978c498
sumit bisht <sumitbisht1987@gmail.com>
2013-07-11 12:01:39 +0530

    removed default index page and provided custom routes for users

--- config/routes.rb
+++ config/routes.rb
@@ -53,6 +53,7 @@ SimpleAuth::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   root :to => 'home#index'
+  match "users" => "home#index"
 
   # See how all your routes lay out with "rake routes"
 
