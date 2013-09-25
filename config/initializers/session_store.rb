29375e415716b13d77762e94d490b389982c6e88
Sumit Bisht <sumitbisht1987@gmail.com>
2013-09-25 15:44:49 +0530

    updated changes to provide backwards compatibility with rails 3

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-SimpleAuth::Application.config.session_store :cookie_store, key: '_simpleAuth_session'
+SimpleAuth::Application.config.session_store :cookie_store, :key=> '_SimpleAuth_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
