6cd0c767cdfa64c3839b015700dbb1f77d1b615b
Sumit Bisht <sumitbisht1987@gmail.com>
2013-10-12 22:38:48 +0530

    added devise helper to fix the error occuring in the devise test

--- test/functional/home_controller_test.rb
+++ test/functional/home_controller_test.rb
@@ -1,8 +1,9 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
+  include Devise::TestHelpers
   test "should get index" do
-    get :index
+  	get :index
     assert_response :success
   end
 
